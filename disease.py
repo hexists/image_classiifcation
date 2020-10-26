a0b291aaaf4c5637a292785e1f636f63dcab11c0
hexists <hexists@daum.net>
2020-10-26 20:24:36 +0900

    add tensorboard, progress bar

--- disease.py
+++ disease.py
@@ -3,20 +3,21 @@
 
 # ref: https://tutorials.pytorch.kr/beginner/blitz/cifar10_tutorial.html
 
-# In[1]:
-
-
 import torch
 import torchvision
 import torchvision.transforms as transforms
 from torchvision import datasets, transforms
 import torch.utils.data as data_utils
-
-
-# In[14]:
+import sys
 
 
 batch_size = 4
+log_interval = 200
+no_cuda = True
+use_cuda = not no_cuda and torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+num_iters = 10
+
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
@@ -29,7 +30,7 @@ transform = transforms.Compose([
 train_data = datasets.ImageFolder(root="images/", transform=transform)
 
 test_len = int(len(train_data) * 0.2)
-train_data, test_data = data_utils.random_split(train_data, (len(train_data) - valid_len, valid_len))
+train_data, test_data = data_utils.random_split(train_data, (len(train_data) - test_len, test_len))
 
 trainloader = data_utils.DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=2)
 testloader = data_utils.DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=2)
@@ -49,9 +50,6 @@ for i, data in enumerate(testloader):
 classes = ('10_20', '11_14', '13_1', '13_15', '13_16', '13_17', '13_18', '13_20', '13_6', '13_9', '3_20', '3_5', '4_11', '4_2', '4_7', '5_8', '7_1', '7_20', '8_6', '8_9')
 
 
-# In[15]:
-
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -68,13 +66,10 @@ def imshow(img):
 dataiter = iter(trainloader)
 images, labels = dataiter.next()
 
-# 이미지 보여주기
-imshow(torchvision.utils.make_grid(images))
-# 정답(label) 출력
-print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
-
-# In[19]:
+# # 이미지 보여주기
+# imshow(torchvision.utils.make_grid(images))
+# # 정답(label) 출력
+# print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 
 import torch.nn as nn
@@ -102,53 +97,49 @@ class Net(nn.Module):
 
 
 net = Net()
-
-
-# In[20]:
-
-
 import torch.optim as optim
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
 
-# In[26]:
+def print_progress(msg, progress):
+    max_progress = int((progress*100)/2)
+    remain=50-max_progress
+    buff="{}\t[".format( msg )
+    for i in range( max_progress ): buff+="⬛"
+    buff+="⬜"*remain
+    buff+="]:{:.2f}%\r".format( progress*100 )
+    sys.stderr.write(buff)
 
 
-log_interval = 200
-no_cuda = True
-use_cuda = not no_cuda and torch.cuda.is_available()
-device = torch.device("cuda" if use_cuda else "cpu")
-
-def test(log_interval, model, device, test_loader):
+def test(log_interval, model, criterion, device, test_loader):
     model.eval()
-    test_loss = 0
+    test_losses = []
     correct = 0
     with torch.no_grad():
-        for data, target in test_loader:
+        for i, (data, target) in enumerate(test_loader):
             data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()
-            pred = output.argmax(dim=1, keepdim=True)
+            outputs = model(data)
+            test_loss = criterion(outputs, labels)
+            test_losses.append(test_loss.item())
+            pred = outputs.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
 
-    test_loss /= len(test_loader.dataset)
+            progress = (i + 1) / float(len(test_loader))
+            print_progress('test', progress)
+        print(file=sys.stderr)
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format
-          (test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
-
-
-# In[27]:
+    return np.mean(test_losses), 100. * correct / len(test_loader.dataset)
 
 
-for epoch in range(2):   # 데이터셋을 수차례 반복합니다.
-
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
+def train(log_interval, model, criterion, device, train_loader, optimizer, epoch):
+    model.train()
+    running_losses = []
+    for i, data in enumerate(train_loader):
         # [inputs, labels]의 목록인 data로부터 입력을 받은 후;
         inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
 
         # 변화도(Gradient) 매개변수를 0으로 만들고
         optimizer.zero_grad()
@@ -156,30 +147,36 @@ for epoch in range(2):   # 데이터셋을 수차례 반복합니다.
         # 순전파 + 역전파 + 최적화를 한 후
         outputs = net(inputs)
         loss = criterion(outputs, labels)
+        running_losses.append(loss.item())
         loss.backward()
         optimizer.step()
 
-        # 통계를 출력합니다.
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
-            
-    test(log_interval, net, device, testloader)
+        progress = (i + 1) / float(len(train_loader))
+        print_progress('train', progress)
+    print(file=sys.stderr)
 
+    return np.mean(running_losses)
+
+
+from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
+date_time = datetime.now().strftime('%Y%m%d%H%M')
+writer = SummaryWriter('./runs/{}'.format(date_time))
+
+
+for epoch in range(1, num_iters + 1):   # 데이터셋을 수차례 반복합니다.
+    tr_loss = train(log_interval, net, criterion, device, trainloader, optimizer, epoch)
+    te_loss, te_acc = test(log_interval, net, criterion, device, testloader)
+
+    print("{} / {}\ttrain loss : {:.4f}, test loss: {:.4f} test acc: {:.4f}\n".format(epoch, num_iters, tr_loss, te_loss, te_acc), file=sys.stderr)
+
+    writer.add_scalar('{}/{}'.format('loss', 'train'), tr_loss, epoch)
+    writer.add_scalar('{}/{}'.format('loss', 'test'), te_loss, epoch)
+    writer.add_scalar('{}/{}'.format('acc', 'test'), te_acc, epoch)
+
+writer.close()
 print('Finished Training')
-
-
-# In[8]:
 
 
 PATH = './disease_net.pth'
 torch.save(net.state_dict(), PATH)
-
-
-# In[ ]:
-
-
-
-

2b6c03f74963b54a6f432836bec0c4e12bca9202
hexists <hexists@daum.net>
2020-10-27 18:32:24 +0900

    update

--- disease.py
+++ disease.py
@@ -18,7 +18,7 @@ use_cuda = not no_cuda and torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 
 transform = transforms.Compose(
-    [transforms.Resize(32),       # 한 축을 128로 조절하고
+    [transforms.Resize(128),       # 한 축을 128로 조절하고
      transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
@@ -84,14 +84,14 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc1 = nn.Linear(16 * 29 * 29, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 20)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        x = x.view(-1, 16 * 29 * 29)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)

bd1ffd4eca5a8cdd66c4cb2fc34ac9e3323b75c6
hexists <hexists@daum.net>
2020-10-28 01:01:38 +0900

    add resnet18.py

---
+++ resnet18.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python3
+
+# https://tutorials.pytorch.kr/beginner/transfer_learning_tutorial.html
+
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+# 학습을 위해 데이터 증가(augmentation) 및 일반화(normalization)
+# 검증을 위한 일반화
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+'''
+data_dir = 'data/hymenoptera_data'
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train', 'val']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                             shuffle=True, num_workers=4)
+              for x in ['train', 'val']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+class_names = image_datasets['train'].classes
+'''
+
+transform = transforms.Compose(
+    [transforms.Resize(256),
+     transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+data = datasets.ImageFolder(root='./images', transform=transform)
+class_names = data.classes
+
+valid_len = int(len(data) * 0.2)
+import torch.utils.data as data_utils
+train_data, valid_data = data_utils.random_split(data, (len(data) - valid_len, valid_len))
+
+train_loader = data_utils.DataLoader(train_data, batch_size=4, shuffle=True, num_workers=2)
+valid_loader = data_utils.DataLoader(valid_data, batch_size=4, shuffle=False, num_workers=2)
+
+dataloaders = {'train': train_loader, 'val': valid_loader}
+dataset_sizes = {'train': len(data) - valid_len, 'val': valid_len}
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # 각 에폭(epoch)은 학습 단계와 검증 단계를 갖습니다.
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # 모델을 학습 모드로 설정
+            else:
+                model.eval()   # 모델을 평가 모드로 설정
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # 데이터를 반복
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # 매개변수 경사도를 0으로 설정
+                optimizer.zero_grad()
+
+                # 순전파
+                # 학습 시에만 연산 기록을 추적
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # 학습 단계인 경우 역전파 + 최적화
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # 통계
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # 모델을 깊은 복사(deep copy)함
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # 가장 나은 모델 가중치를 불러옴
+    model.load_state_dict(best_model_wts)
+    return model
+
+'''
+# 미리 학습된 ConvNet의 파라미터 값을 초기 값으로 하여 이후의 모델을 새로운 데이터 셋에 대해서 새롭게 학습하는 시나리오
+
+model_ft = models.resnet18(pretrained=True)
+num_ftrs = model_ft.fc.in_features
+# 여기서 각 출력 샘플의 크기는 2로 설정합니다.
+# 또는, nn.Linear(num_ftrs, len (class_names))로 일반화할 수 있습니다.
+model_ft.fc = nn.Linear(num_ftrs, len(class_names))
+
+model_ft = model_ft.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+# 모든 매개변수들이 최적화되었는지 관찰
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+# 7 에폭마다 0.1씩 학습률 감소
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                       num_epochs=25)
+'''
+
+# 미리 학습된 모델은 그대로 고정된 Feature로 두고, 맨 위에 Fully Connected Layer만 추가
+model_conv = torchvision.models.resnet18(pretrained=True)
+for param in model_conv.parameters():
+    param.requires_grad = False
+
+# 새로 생성된 모듈의 매개변수는 기본값이 requires_grad=True 임
+num_ftrs = model_conv.fc.in_features
+model_conv.fc = nn.Linear(num_ftrs, len(class_names))
+
+model_conv = model_conv.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+# 이전과는 다르게 마지막 계층의 매개변수들만 최적화되는지 관찰
+optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
+
+# 7 에폭마다 0.1씩 학습률 감소
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+
+model_conv = train_model(model_conv, criterion, optimizer_conv,
+                         exp_lr_scheduler, num_epochs=25)
